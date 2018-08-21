e425833712af85d20fac07d26e7d75a7edc3b91f
Jonathan Rayback <jrayback@gmail.com>
2018-08-21 13:59:33 -0600

    remove arrow notation in mocha tests, per mocha best practice

--- test/tests.js
+++ test/tests.js
@@ -3,11 +3,11 @@ const expect = require('chai').expect
 const algorithm = require('../algorithms')
 
 // verify mocha is working and that basic plumbing into algorithm module is working
-describe('[Baseline Tests]', () => {
-  it('Should equal true when comparing true to true', () => {
+describe('[Baseline Tests]', function () {
+  it('Should equal true when comparing true to true', function () {
     expect(true).to.equal(true)
   })
-  it('Should return expected test greeting', () => {
+  it('Should return expected test greeting', function () {
     const expected = 'hello world!'
     const actual = algorithm.returnGreeting()
     expect(actual).to.equal(expected)
@@ -19,24 +19,24 @@ function getActual (originSquare, destinationSquare) {
   return algorithm.numChessMovesBishop(originSquare, destinationSquare)
 }
 
-describe('[Number of Chess Moves Tests]', () => {
-  it('Should throw a range error if the passed in number for either square is out of range', () => {
+describe('[Number of Chess Moves Tests]', function () {
+  it('Should throw a range error if the passed in number for either square is out of range', function () {
     // try both ways
-    expect(() => {
+    expect(function () {
       algorithm.numChessMovesBishop(104, 0)
     }).to.throw(RangeError)
-    expect(() => {
+    expect(function () {
       algorithm.numChessMovesBishop(0, 203)
     }).to.throw(RangeError)
   })
-  it('Should return 0 when sent identical squares', () => {
+  it('Should return 0 when sent identical squares', function () {
     const originSquare = 1
     const destinationSquare = 1
     const expected = 0
     const actual = getActual(originSquare, destinationSquare)
     expect(actual).to.equal(expected)
   })
-  it('Should return 1 when the destination square is directly diagonal from the origin square', () => {
+  it('Should return 1 when the destination square is directly diagonal from the origin square', function () {
     const expected = 1
     let actual
     // Try several iterations of diagonal moves in different lengths and directions
@@ -49,7 +49,7 @@ describe('[Number of Chess Moves Tests]', () => {
     actual = getActual(25, 4)
     expect(actual).to.equal(expected)
   })
-  it('Should return -1 if the destination square is an opposite color from the origin square', () => {
+  it('Should return -1 if the destination square is an opposite color from the origin square', function () {
     const expected = -1
     let actual
     // test a few just to be sure
@@ -60,7 +60,7 @@ describe('[Number of Chess Moves Tests]', () => {
     actual = getActual(56, 54)
     expect(actual).to.equal(expected)
   })
-  it('Should return 2 in any case other than squares directly diagonal or squares of opposite color', () => {
+  it('Should return 2 in any case other than squares directly diagonal or squares of opposite color', function () {
     const originSquare = 0
     const destinationSquare = 11
     const expected = 2
