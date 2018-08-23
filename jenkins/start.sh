2a339aee62e0d754ef9a690a8e872a33a548757c
Jonathan Rayback <jrayback@gmail.com>
2018-08-23 17:31:53 -0600

    Add comment to start script describing one-time process to configure Jenkins server

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# 
+# NOTE: The first time this container is started, you will have to configure
+# the Jenkins server and import the job(s) with the config files under the
+# jobs directory. After that, the configuration will persist because of the
+# docker volume we're mounting.
+#
 docker run -d --name jmrjenkins -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jrayback/jenkins:latest
